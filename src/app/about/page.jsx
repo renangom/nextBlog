00a8b067e67975c34613552c4a5e033c9b76c1e7
Renan <renan23eduardo@gmail.com>
2024-02-16 23:13:34 -0300

    styling about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,34 @@
 import Image from "next/image"
+import styles from './about.module.css'
+
 
 const AboutPage = () => {
     return (
-        <div> 
-            <Image src="/about.png" alt="" width={550} height={550} />
+        <div className={styles.container}> 
+            <div className={styles.textContainer}>
+                <h2 className={styles.subtitle}>About Agency</h2>
+                <h1 className={styles.title}>We create digital ideas that are bigger, bolder, braver and better</h1>
+                <p className={styles.dec}>
+                    We create digital ideas that are bigger, bolder, braver and better. We believe in good ideas flexibility and precision. We're world's Our Special Team best consulting & finance solution provider. Wide range of web and software development services
+                </p>
+                <div className={styles.boxes}>
+                    <div className={styles.box}>
+                        <h1>10 K+</h1>
+                        <p>Year of experience</p>
+                    </div>
+                    <div className={styles.box}>
+                        <h1>10 K+</h1>
+                        <p>Year of experience</p>
+                    </div>
+                    <div className={styles.box}>
+                        <h1>10 K+</h1>
+                        <p>Year of experience</p>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.imgContainer}>
+                <Image src='/about.png' alt="" fill className={styles.image} />
+            </div>
         </div>
     )
 }
